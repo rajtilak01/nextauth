23e13244d85b5a17d4becbe265ae4bb92338e291
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2024-03-25 21:53:16 +0530

    signup route

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,23 +1,42 @@
+import User from '@/models/userModel';
 import nodemailer from 'nodemailer'
+import bcryptjs from "bcryptjs"
+
+require("dotenv").config();
 
 export const sendEmail = async ({email, emailType, userId}:any) => {    
     try {
+        const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+        if(emailType === "VERIFY"){
+          await User.findByIdAndUpdate(userId,
+            {verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000}
+            // {verifyToken}
+          )
+        } else if(emailType = "RESET"){
+          await User.findByIdAndUpdate(userId, 
+            {
+              forgotPasswordToken: hashedToken,
+              forgotPasswordTokenExpiry: Date.now() + 3600000
+            })
+        }
+
         const transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false, // Use `true` for port 465, `false` for all other ports
-            auth: {
-              user: "maddison53@ethereal.email",
-              pass: "jn7jnAPss4f63QBp6D",
-            },
-          });
+          host: "sandbox.smtp.mailtrap.io",
+          port: 2525,
+          auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASSWORD
+          }
+        });
 
           const info = await transporter.sendMail({
-            from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
+            from: 'rajtilakpandey01@gmail.com', // sender address
             to: email, // list of receivers
             subject: emailType === 'VERIFY' ? "Verify your email": "Reset your passwword", // Subject line
-            text: "Hello world?", // plain text body
-            html: "<b>Hello world?</b>", // html body
+             
+            html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "Verify your email" : "reset your password"} or copy and paste the link below in your browser.
+            <br> ${process.env.domain}/verifyemail?token=${hashedToken}
+            </p>`, // html body
           });
     } catch (error) {
         
