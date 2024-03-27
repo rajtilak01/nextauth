ac26888b90b5728e3e5214e264d8f7edc2dc546b
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2024-03-27 15:34:58 +0530

    login and logout routes added

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,55 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import {NextRequest, NextResponse} from "next/server"
+import bcryptjs from "bcryptjs"
+import jwt from "jsonwebtoken";
+
+connect();
+
+export async function POST(request: NextRequest){
+    try {
+        const reqBody = await request.json();
+        const {email, password} = reqBody;
+        console.log(reqBody);
+
+        const user = await User.findOne({email});
+
+        if(!user){
+            return NextResponse.json({error: "User does not exists"}, {status: 500})
+        }
+        
+        console.log("User exists");
+
+        const validPassword = await bcryptjs.compare(password, user.password);
+
+        if(!validPassword){
+            return NextResponse.json({error: "check your credentials"}, {status: 400});
+        }
+
+        const tokenData = {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+        }
+        if (!process.env.TOKEN_SECRET) {
+            throw new Error('Token secret is not defined');
+        }
+
+        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET, {expiresIn: '1d'});
+        
+        const response = NextResponse.json({
+            message: "Logged in success",
+            success: true,
+        })
+
+        response.cookies.set("token", token, {
+            httpOnly: true,
+        });
+
+        return response;
+
+
+    } catch (error : any) {
+        return NextResponse.json({error: error.message}, {status: 500});
+    }
+}
